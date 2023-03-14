e8ccfcd5760e16b190df3bb2430d83f769f43b31
notestack <estarck@me.com>
2023-03-14 10:48:40 +0100

    change client

--- Client.hpp
+++ Client.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: estarck <estarck@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 10:32:36 by estarck           #+#    #+#             */
-/*   Updated: 2023/03/15 18:53:20 by estarck          ###   ########.fr       */
+/*   Created: 2023/03/13 14:18:05 by estarck           #+#    #+#             */
+/*   Updated: 2023/03/14 10:28:53 by estarck          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,35 @@
 #include <sstream>
 #include <string>
 
+#include "../include/ParsConfig.hpp"
+#include "../include/Server.hpp"
 
-#define MAX_REQUEST_SIZE 2048
-
-struct Client
+class Client
 {
-    /* Socket et contexte d'adressage du client */
-	int					_csock;
-	sockaddr_storage	_csin;
-	socklen_t			_crecsize;
-	timeval				_lastGetTime;
+	public :
+		Client(unsigned int nbrServer, std::vector<ParsConfig *> &config, std::vector<Server *> &server);
+		Client(const Client &srcs);
+		~Client();
 
-	char				_recBuffer[MAX_REQUEST_SIZE + 1];
-	int					_recSize;
-};//struct Client
+		Client &operator=(const Client &srcs);
+
+	private :
+    	/* Socket et contexte d'adressage du client */
+		int			_csock;
+		sockaddr_in	_csin;
+		socklen_t	_crecsize;
+
+		/* Gestion des fd */
+		fd_set		_readFds;
+		int			_maxFd;
+
+		/* Serveur */
+		int							_nbrServer;
+		std::vector<ParsConfig *>	_config;
+		std::vector<Server *>		_server;
+
+		void	acceptConnection();
+		
+};//class Client
 
 #endif/* _CLIENT_HPP_ */
